e46c07920423c89fe7a0b665ef6836615ebe8029
Dinesh Kodali <dineshkodali7@gmail.com>
2024-06-22 10:05:18 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Posted in Github Serverless
+          Posted in Github Serverless.
         </p>
         <a
           className="App-link"
