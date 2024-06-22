4117138ad0e64a73498db077738fba6d487d5220
Dinesh Kodali <dineshkodali7@gmail.com>
2024-06-22 10:18:34 +0100

    index added

---
+++ components/index.js
@@ -0,0 +1,13 @@
+// src/IndexPage.js
+import React from 'react';
+
+const IndexPage = () => {
+  return (
+    <div>
+      <h1>Welcome to the Index Page</h1>
+      <p>This is the main page of your React application.</p>
+    </div>
+  );
+};
+
+export default IndexPage;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import IndexPage from './IndexPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Posted in Github Serverless.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <IndexPage />
     </div>
   );
 }
