2ecabeb044c9db9193498129c669b54638412ea8
Dinesh Kodali <dineshkodali7@gmail.com>
2024-06-22 10:03:27 +0100

    q

--- src/App.js
+++ src/App.js
@@ -7,8 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Posted in Github Serverless
+          Edit <code>src/App.js</code> and save to reload. Posted in Github Serverless
         </p>
         <a
           className="App-link"
