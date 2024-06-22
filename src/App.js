3d9f3da31a144fcbc7e0eaabf3002d566caf3470
Dinesh Kodali <dineshkodali7@gmail.com>
2024-06-22 10:20:40 +0100

    aa

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-// src/App.js
-import React from 'react';
-import IndexPage from './IndexPage';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <IndexPage />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Posted in Github Serverless.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
